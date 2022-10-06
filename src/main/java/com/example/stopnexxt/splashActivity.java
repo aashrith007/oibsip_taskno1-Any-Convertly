2186af6eabc412085c9860af56d3bc27f8a73f53
aashrith007 <112975403+aashrith007@users.noreply.github.com>
2022-10-06 19:22:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/stopnexxt/splashActivity.java
@@ -0,0 +1,24 @@
+package com.example.stopnexxt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashActivity.this, MainActivity.class));
+            }
+        },2000);
+    }
+}
